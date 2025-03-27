2acdcdec44f17a90aeaba0ff767053dd21875bb8
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-03-26 22:37:29 -0300

    new exercise

---
+++ arithmeticOperators/arithmeticOperators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmeticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int friends = 10;
+            //friends = friends + 2;
+            //friends += 2;
+            //friends++; //apenas +1
+
+            //friends = friends - 1;
+            //friends -= 1;
+            //friends--;
+
+            //friends = friends * 2;
+            //friends *= 2;
+
+            //friends = friends / 2; //divisão inteira, pois a variável é uma int
+            //friends /= 2;
+
+            int remainder = friends % 3;  //módulo = resto da divisão
+
+            Console.WriteLine(remainder);
+            Console.WriteLine(friends);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arithmeticOperators/arithmeticOperators/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("arithmeticOperators")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("arithmeticOperators")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18279cdc-27c8-4a35-b0e3-974d45d903aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
