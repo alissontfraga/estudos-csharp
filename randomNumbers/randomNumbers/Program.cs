2b32a1209f16f29fa6c6d647872e29294785d141
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-03-27 23:33:53 -0300

    New Projects

---
+++ randomNumbers/randomNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            //int num = random.Next(1, 7);  //de 1 a 6          //0 over 2billion  
+            //double num = random.NextDouble();//gera num > 0 e < 1, gera um número decimal
+            
+            int num1 = random.Next(1, 7);
+            int num2 = random.Next(1, 7);
+            int num3 = random.Next(1, 7);
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+            Console.ReadKey();
+        }
+    }
+}
